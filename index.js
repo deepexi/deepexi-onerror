a34bc0b8467955d455d67dbd08ceac230e7b7b93
taccisum <liaojinfeng@deepexi.com>
2019-06-18 21:44:49 +0800

    extract fn abount build body as config

--- index.js
+++ index.js
@@ -4,26 +4,36 @@ module.exports = opt => {
   if (!opt) {
     opt = {}
   }
+  const options = _.merge({
+    stack: {},
+    status: {
+      biz: 406
+    },
+    builder: {
+      body (success, msg, stack) {
+        return {
+          success,
+          message: msg,
+          stack
+        }
+      }
+    }
+  }, opt)
 
   return {
     json (err, ctx) {
-      const options = _.merge({
-        stack: {},
-        status: {
-          biz: 406
-        }
-      }, opt)
-
-      const body = { success: false }
+      const success = false
+      let msg
+      let stack
 
       const status = err.unsafeStatus || err.status || 500 // 支持自定义http状态
 
       const bizErrorStatus = options.status.biz
 
       if (err.status === bizErrorStatus) {
-        body.message = err.message
+        msg = err.message
       } else {
-        body.message = 'Internal Server Error'
+        msg = 'Internal Server Error'
       }
 
       let outStack = options.stack.out
@@ -34,11 +44,11 @@ module.exports = opt => {
       }
 
       if (outStack) {
-        body.stack = err.stack
+        stack = err.stack
       }
 
       ctx.status = status
-      ctx.body = body
+      ctx.body = options.builder.body(success, msg, stack)
     }
   }
 }
